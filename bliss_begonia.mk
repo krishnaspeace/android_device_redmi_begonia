9cfb6d9b188463d21132a65a4ee0cbe2a5265a03
krishnaspeace <krishnasaarav@gmailcom>
2024-05-23 02:41:47 +0700

    begonia: add lieca cam

--- bliss_begonia.mk
+++ bliss_begonia.mk
@@ -25,6 +25,7 @@ BLISS_BUILDTYPE := UNOFFICIAL
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
+$(call inherit-product-if-exists, vendor/MiuiCameraLeica/config.mk)
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
