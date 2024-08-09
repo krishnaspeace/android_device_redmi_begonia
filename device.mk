04b77251e934ce5c771c6f21b0ee259b0499105f
Saikrishna1504 <saikrishna26918@gmail.com>
2024-08-09 10:35:09 +0530

    begonia: Build more missing VNDK blobs
    E audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library "android.hardware.audio.sounddose-vendor-impl.so" not found
    
    E APUWareServer: unable to open Apusys library APUWareApusysServer.so, with error dlopen failed: library "APUWareApusysServer.so" not found
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.common@6.0-util \
+    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.soundtrigger@2.3-impl \
     android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth@1.0.vendor \
@@ -217,6 +218,10 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.1.vendor \
     android.hardware.neuralnetworks@1.2.vendor \
     android.hardware.neuralnetworks@1.3.vendor
+
+# APUWareApusysServer
+PRODUCT_PACKAGES += \
+    APUWareApusysServer.vendor
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
